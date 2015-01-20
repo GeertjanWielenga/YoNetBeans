cc977e2d51b28da77b7d7704818be3b61e69eeb4
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-01-20 06:56:39 +0100

    yo ko

--- Yo/src/org/netbeans/modules/yo/YoAction.java
+++ Yo/src/org/netbeans/modules/yo/YoAction.java
@@ -1,12 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.netbeans.modules.yo;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import org.netbeans.api.extexecution.ExecutionDescriptor;
+import org.netbeans.api.extexecution.ExecutionService;
+import org.netbeans.api.extexecution.ExternalProcessBuilder;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
@@ -25,6 +26,29 @@ public final class YoAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO implement action body
+        NotifyDescriptor.InputLine inputLine = new NotifyDescriptor.InputLine("Folder: ", "Yeoman");
+        Object result = DialogDisplayer.getDefault().notify(inputLine);
+        if (NotifyDescriptor.OK_OPTION == result) {
+            String inputText = inputLine.getInputText();
+//            String yoPath = inputText;
+            String yoPath = "C:\\\\Users\\\\gwieleng\\\\AppData\\\\Roaming\\\\npm\\\\yo.cmd";
+            ExternalProcessBuilder processBuilder = new ExternalProcessBuilder(yoPath).
+                    addArgument("ko").
+                    //                 addArgument("-m").
+                    // addArgument(namespaceName + "/" + methodName).
+                    workingDirectory(new File(inputText));
+            ExecutionDescriptor descriptor = new ExecutionDescriptor().
+                    frontWindow(true).
+                    showProgress(true).
+                    showSuspended(true).
+                    inputVisible(true).
+                    controllable(true);
+            ExecutionService service = ExecutionService.newService(
+                    processBuilder,
+                    descriptor,
+                    "Yeoman");
+            service.run();
+        }
     }
+
 }
