604aef85bf613fc6bd730115954409c5a9079e93
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-02-02 13:47:51 +0100

    status bar display description of selected generator

--- Yo/src/org/netbeans/modules/yo/template2/AllGeneratorsPanel.java
+++ Yo/src/org/netbeans/modules/yo/template2/AllGeneratorsPanel.java
@@ -1,18 +1,22 @@
 package org.netbeans.modules.yo.template2;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import org.openide.awt.StatusDisplayer;
 import org.openide.explorer.ExplorerManager;
 import org.openide.explorer.view.OutlineView;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
+import org.openide.nodes.Node;
 
 public class AllGeneratorsPanel extends JPanel implements ExplorerManager.Provider {
 
@@ -20,7 +24,7 @@ public class AllGeneratorsPanel extends JPanel implements ExplorerManager.Provid
     private final AllGeneratorsChildFactory ycf;
 
     public AllGeneratorsPanel() {
-        setLayout(new BorderLayout());
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1), "All Generators"));
         em = new ExplorerManager();
         OutlineView ov = new OutlineView("Status");
@@ -30,8 +34,17 @@ public class AllGeneratorsPanel extends JPanel implements ExplorerManager.Provid
         ycf = new AllGeneratorsChildFactory();
         Children kids = Children.create(ycf, true);
         AbstractNode rootNode = new AbstractNode(kids);
-        add(ov, BorderLayout.CENTER);
-        add(createInstalledGeneratorsCheckbox(), BorderLayout.SOUTH);
+        add(ov);
+        em.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                Node[] selectedNodes = em.getSelectedNodes();
+                for (Node node : selectedNodes) {
+                    StatusDisplayer.getDefault().setStatusText(node.getLookup().lookup(YeomanGeneratorObject.class).getDescription());
+                }
+            }
+        });
+        add(createInstalledGeneratorsCheckbox());
         em.setRootContext(rootNode);
     }
 
@@ -42,7 +55,7 @@ public class AllGeneratorsPanel extends JPanel implements ExplorerManager.Provid
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (cb.isSelected()) {
-                    ycf.defineFilter(true,"Making only installed generators visible...");
+                    ycf.defineFilter(true, "Making only installed generators visible...");
                 } else {
                     ycf.defineFilter(false, "Making all generators visible...");
                 }
